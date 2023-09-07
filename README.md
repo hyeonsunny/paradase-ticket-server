8614a9742a371aea585886b83f4638883c84373b
LimJiNa <jn4624@gmail.com>
2023-09-07 23:36:53 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# paradase-ticket-server
---
+++ src/main/java/com/app/paradase/ParadaseTicketServerApplication.java
@@ -0,0 +1,13 @@
+package com.app.paradase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParadaseTicketServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParadaseTicketServerApplication.class, args);
+    }
+
+}
